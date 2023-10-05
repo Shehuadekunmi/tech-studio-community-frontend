5327d941c6248c24b03880cfb65d27de190ca453
Olatunbosun Solomon <olatunbosunsolomon5@outlook.com>
2023-10-05 15:07:21 +0100

    update on landing-page

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,325 @@
+import React from "react";
+import "../styles/projects.css";
+import image1 from "../assets/image1.png";
+import icon1 from "../assets/icon1.png";
+import image2 from "../assets/image2.png";
+import icon2 from "../assets/icon2.png";
+import image3 from "../assets/image3.png";
+import icon3 from "../assets/icon3.png";
+import { AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineEye } from "react-icons/ai";
+import { Container } from "react-bootstrap";
+
+const Projects = () => {
+  return (
+    <Container>
+      <div className="mt-5">
+        <section>
+          <div className="d-flex justify-content-between filter">
+            <h3>Community Projects</h3>
+            <select name="" id="">
+              <option value="">Filter By</option>
+              <option value="">Data Analysis</option>
+              <option value="">Web Development</option>
+              <option value="">Product Design</option>
+            </select>
+          </div>
+          <hr />
+        </section>
+        <section>
+          <div className="d-flex justify-content-evenly flex-wrap">
+            <div className="mt-5">
+              <img src={image1} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon1} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image2} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon2} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <div className="gap-3 d-flex">
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex gap-1">
+                    <p className="d-flex fs-2">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <img src={image3} alt="" className="w-100 h-100" />
+              <div className="d-flex justify-content-between mt-2">
+                <img src={icon3} alt="" className="w-auto h-25" />
+                <p className="w-auto fs-6 ">multiple owners</p>
+                <div className="gap-1 d-flex">
+                  <div className="d-flex  ">
+                    <p className="d-flex fs-6 p-1">
+                      {" "}
+                      <AiOutlineHeart />
+                    </p>
+                    <p>232</p>
+                  </div>
+                  <div className="d-flex ">
+                    <p className="d-flex fs-6 p-1">
+                      {" "}
+                      <AiOutlineEye />
+                    </p>
+                    <p>1.4k</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Container>
+  );
+};
+
+export default Projects;
--- src/pages/lading page/Home.jsx
+++ src/pages/lading page/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/Hero'
 import { Container } from 'react-bootstrap'
+import Projects from '../../components/Projects';
 
 const Home = () => {
   return (
     <div>
       <Container>
         <Hero />
+        <Projects/>
       </Container>
     </div>
   );
