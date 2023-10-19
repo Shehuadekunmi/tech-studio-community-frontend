983a3c7f41d5c55183a260848d318b62e1ebb1dd
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-10-19 10:44:02 +0100

    edited

--- src/components/FindtalentSec1.jsx
+++ src/components/FindtalentSec1.jsx
@@ -5,7 +5,7 @@ import can from "../assets/can.svg";
 import frame from "../assets/Frame.svg";
 import layer from "../assets/Layer.svg";
 import "../pages/find talent/style.css"
-import Loader from "./Loader"
+import Loader from "../Components/Loader"
 
 const FindtalentSec1 = ({ SetJobPosterData }) => {
   const apiUrl = "https://techstudiocommunity.onrender.com";
